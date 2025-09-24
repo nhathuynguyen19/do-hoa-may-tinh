1af8dbff211c77d858f83ffe4dcd88174a72eeda
nhathuynguyen <huythcsthuyphuong73@gmail.com>
2025-09-24 15:10:59 +0700

    Update my-lib.cpp

--- my-lib.cpp
+++ my-lib.cpp
@@ -3,13 +3,31 @@ struct resultTable {
     int p;
     int x;
     int y;
+    resultTable *next;
 }
 
 struct Point {
     int x;
     int y;
+    Point *next;
 }
 
-resultTable* bresLine(int xa, int ya, in) {
+Point* swapPoint(double alpha, Point a, Point b) {
+    if (alpha >= 0 && alpha <= 1) {
+        Point *rs = new Point();
+        rs->x = a.x;
+        rs->y = a.y;
+        Point *rsb = new Point();
+        rsb->x = b.x;
+        rsb->y = b.y;
+        rs->next = rsb;
+        return rs;
+    }
+}
 
+resultTable* bresLine(Point a, Point b) {
+    int dx = b.x - a.x;
+    int dy = b.y - a.y;
+    double alpha = (double) dy / dx;
+    
 }
